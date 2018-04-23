932f2b59151f69857d1a8581608c85ca129af9a6
Keks <keks@htmlacademy.ru>
2018-04-23 17:38:46 +0300

    :kissing_cat: Добавляет сборку Less на Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "source/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")()
+          ]
+        },
+        src: "source/css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "source/*.html",
+            "source/css/*.css"
+          ]
+        },
+        options: {
+          server: "source/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss"]
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+};
