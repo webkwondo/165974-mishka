6d3a1caadd3b89b4318dd7b63791054c8c1f04a4
Elena Markova <webkwondo.com@gmail.com>
2018-05-19 14:55:28 +0300

    Адаптивность. Вся основная верстка

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+document.createElement( "picture" );
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -17,36 +19,38 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-var link = document.querySelector(".featured-product__button");
-var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__close");
+if ( document.querySelector(".modal") ) {
 
-if (link) {
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal--show");
-  });
-}
+  var popup = document.querySelector(".modal");
+  var close = popup.querySelector(".modal__close");
 
-if (close) {
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal--show");
-  });
-}
+  if (close) {
+    close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal--show");
+    });
+  }
 
-var link2 = document.querySelectorAll(".product__add-to-cart");
+  var link = document.querySelector(".featured-product__button");
 
-if (link2) {
-  // link2.addEventListener("click", function (evt) {
-  //   evt.preventDefault();
-  //   popup.classList.add("modal--show");
-  // });
-
-  for (var i = 0; i < link2.length; i++) {
-      link2[i].addEventListener("click", function (evt) {
+  if (link) {
+    link.addEventListener("click", function (evt) {
       evt.preventDefault();
       popup.classList.add("modal--show");
     });
   }
+
+  var link2 = document.querySelectorAll(".product__add-to-cart");
+
+  if (link2) {
+
+    for (var i = 0; i < link2.length; i++) {
+        link2[i].addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal--show");
+      });
+    }
+
+  }
+
 }
